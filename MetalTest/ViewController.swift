33e6c926842e2ba2f6d23b61d16f155a3d4e9215
Zedd <zedd.0202@kakaocorp.com>
2020-06-12 15:19:38 +0900

    first Commit

---
+++ MetalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalTest
+//
+//  Created by Zedd on 2020/06/12.
+//  Copyright © 2020 Zedd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
